bf425ea86feeb0ca5b60e48352d39f9bd7e73b50
Charles Shorter Jr <c4short27@gmail.com>
2019-04-27 00:48:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays(){
   return ("Happy holidays!")
 }
 
-//function that returns parameter
+//function that returns a string and parameter via interpolation
 function happyHolidaysTo(name){
   return ("Happy holidays, " + name + "!")
 }
